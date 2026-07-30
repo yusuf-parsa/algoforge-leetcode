b06c7c686e33e0d6cdb11bde871d09050b8546fb
yusuf-parsa <info.yusuf2025@gmail.com>
2026-07-31 01:26:06 +0600

    Solve 53 — Maximum Subarray

---
+++ solutions/dynamic-programming/java/0053-maximum-subarray.java
@@ -0,0 +1,24 @@
+/**
+ * LeetCode 53: Maximum Subarray
+ * https://leetcode.com/problems/maximum-subarray/
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maximumSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int num : nums) {
+            currentSum += num;
+            maximumSum = Math.max(maximumSum, currentSum);
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        return maximumSum;
+    }
+}
