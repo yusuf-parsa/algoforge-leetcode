ebcb48fadf47afaf7163aca21d667d3430815347
yusuf-parsa <info.yusuf2025@gmail.com>
2026-07-31 00:38:12 +0600

    Solve 9 — Palindrome Number

---
+++ solutions/math/java/0009-palindrome-number.java
@@ -0,0 +1,22 @@
+// Problem: https://leetcode.com/problems/palindrome-number/
+// Time: O(log x)
+// Space: O(1)
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int temp = x;
+        long reversed = 0;
+
+        while (temp > 0) {
+            int lastDigit = temp % 10;
+            reversed = reversed * 10 + lastDigit;
+            temp /= 10;
+        }
+
+        return reversed == x;
+    }
+}
