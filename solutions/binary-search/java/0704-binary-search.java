742bf543d427a9813d4594e52ac5a68119bf30ab
yusuf-parsa <info.yusuf2025@gmail.com>
2026-07-31 01:29:54 +0600

    Solve 704 — Binary Search

---
+++ solutions/binary-search/java/0704-binary-search.java
@@ -0,0 +1,29 @@
+/**
+ * LeetCode 704: Binary Search
+ * https://leetcode.com/problems/binary-search/
+ *
+ * Time: O(log n)
+ * Space: O(1)
+ */
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            }
+
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+}
