daa3aa5668c54e6a1e46b72ddf256826aacea501
yusuf-parsa <info.yusuf2025@gmail.com>
2026-07-31 00:38:36 +0600

    Solve 169 — Majority Element

---
+++ solutions/arrays/java/0169-majority-element.java
@@ -0,0 +1,24 @@
+// Problem: https://leetcode.com/problems/majority-element/
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
