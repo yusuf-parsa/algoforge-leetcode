13b895a145d2b71e1557bfa6ef15ae407f30547d
yusuf-parsa <info.yusuf2025@gmail.com>
2026-07-31 01:00:32 +0600

    Solve 344 — Reverse String

---
+++ solutions/strings/java/0344-reverse-string.java
@@ -0,0 +1,21 @@
+/**
+ * LeetCode 344: Reverse String
+ * https://leetcode.com/problems/reverse-string/
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
