21a8356769cc0a02bad1cf257c5ea4f715000bc2
yusuf-parsa <info.yusuf2025@gmail.com>
2026-07-31 01:00:51 +0600

    Solve 680 — Valid Palindrome II

---
+++ solutions/strings/java/0680-valid-palindrome-ii.java
@@ -0,0 +1,38 @@
+/**
+ * LeetCode 680: Valid Palindrome II
+ * https://leetcode.com/problems/valid-palindrome-ii/
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right)
+                        || isPalindrome(s, left, right - 1);
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
